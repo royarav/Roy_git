64df84c783da039b6fe35f59b2ff116a103ab137
royarav <roy_arav@hotmail.com>
2019-07-23 21:35:33 +0300

    Add files via upload

--- Block_Chain_Header.cpp
+++ Block_Chain_Header.cpp
@@ -15,30 +15,30 @@ void Block_Chain_Header::Parse(const char* buffer)
 //	memcpy(buffer)
 	const char* p = buffer;
 
-	Block_Chain_Header::magic_id = read_be32(p);
+	this->magic_id = read_be32(p);
 	p += sizeof(uint32_t);
-	memcpy(Block_Chain_Header::command, p, sizeof(Block_Chain_Header::command));
-	Block_Chain_Header::command[sizeof(Block_Chain_Header::command) - 1] = '\0';
-	p += sizeof(Block_Chain_Header::command);
+	memcpy(this->command, p, sizeof(this->command));
+	this->command[sizeof(this->command) - 1] = '\0';
+	p += sizeof(this->command);
 
-	Block_Chain_Header::length = read_be32(p);
+	this->length = read_be32(p);
 	p += sizeof(uint32_t);
-	Block_Chain_Header::checksum = read_be32(p);
+	this->checksum = read_be32(p);
 	p += sizeof(uint32_t);
 
-	Block_Chain_Header::block_chain_header_length = (p - buffer);
+	this->block_chain_header_length = (p - buffer);
 }
 
 bool Block_Chain_Header::Check_Header() const
 {
-	if (Block_Chain_Header::magic_id != BLOXROUTE_MAGIC_NUM)
+	if (this->magic_id != BLOXROUTE_MAGIC_NUM)
 	{
-		cout << "Unexpected magic num " << Block_Chain_Header::magic_id << endl;
+		cout << "Unexpected magic num " << this->magic_id << endl;
 		return false;
 	}
-	if (strcmp((const char *)Block_Chain_Header::command, BLOCKCHAIN_MSG_BLOCK) != 0)
+	if (strcmp((const char *)this->command, BLOCKCHAIN_MSG_BLOCK) != 0)
 	{
-		cout << "Unexpected command " << Block_Chain_Header::command << endl;
+		cout << "Unexpected command " << this->command << endl;
 		return false;
 	}
 	return true;
@@ -46,10 +46,10 @@ bool Block_Chain_Header::Check_Header() const
 
 void Block_Chain_Header::Show() const
 {
-	cout << "magic_id " << Block_Chain_Header::magic_id << endl;
-	cout << "command " << Block_Chain_Header::command << endl;
-	cout << "length " << Block_Chain_Header::length << endl;
-	cout << "checksum " << Block_Chain_Header::checksum << endl;
+	cout << "magic_id " << this->magic_id << endl;
+	cout << "command " << this->command << endl;
+	cout << "length " << this->length << endl;
+	cout << "checksum " << this->checksum << endl;
 }
 
 
